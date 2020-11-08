a1cbd651a758da647f0025c2d26f9de7cf96001b
Kim-Robin <robin1265@gmail.com>
2020-11-08 11:39:35 -0500

    starting cod e for qr

---
+++ hackrpi2020/lib/Qr/qrGenerator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class QrGenerate extends StatefullWidget {
+  @override
+  State<StatefulWidget> createState() => GeneratePageState();
+}
+
+class GeneratePageState extends State<QrGenerate> {
+  String qrData =
+      "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwitlriwrPPsAhXpnuAKHWQKDyIQPAgI";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: ,)
+  }
+}
